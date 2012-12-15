545d6e2bae2c3e635be05e0d040a1498af9ce14a
Tommi Virtanen <tv@eagain.net>
2012-12-14 16:11:10 -0800

    Run go fix to update for go1.

--- moreio.go
+++ moreio.go
@@ -1,9 +1,6 @@
 package moreio
 
-import (
-	"io"
-	"os"
-	)
+import "io"
 
 type multiplex struct {
 	r io.ReadCloser
@@ -14,15 +11,15 @@ func NewReadWriteCloser(r io.ReadCloser, w io.WriteCloser) *multiplex {
 	return &multiplex{r: r, w: w}
 }
 
-func (m *multiplex) Read(p []byte) (n int, err os.Error) {
+func (m *multiplex) Read(p []byte) (n int, err error) {
 	return m.r.Read(p)
 }
 
-func (m *multiplex) Write(p []byte) (n int, err os.Error) {
+func (m *multiplex) Write(p []byte) (n int, err error) {
 	return m.w.Write(p)
 }
 
-func (m *multiplex) Close() os.Error {
+func (m *multiplex) Close() error {
 	err := m.w.Close()
 	err2 := m.r.Close()
 	if err == nil {
