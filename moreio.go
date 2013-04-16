6e394610e2f6c9dca71067059ebd6de5231f511c
Tommi Virtanen <tv@eagain.net>
2013-04-16 09:11:24 -0700

    Simplify.

--- moreio.go
+++ moreio.go
@@ -3,25 +3,17 @@ package moreio
 import "io"
 
 type multiplex struct {
-	r io.ReadCloser
-	w io.WriteCloser
+	io.ReadCloser
+	io.WriteCloser
 }
 
 func NewReadWriteCloser(r io.ReadCloser, w io.WriteCloser) *multiplex {
-	return &multiplex{r: r, w: w}
-}
-
-func (m *multiplex) Read(p []byte) (n int, err error) {
-	return m.r.Read(p)
-}
-
-func (m *multiplex) Write(p []byte) (n int, err error) {
-	return m.w.Write(p)
+	return &multiplex{ReadCloser: r, WriteCloser: w}
 }
 
 func (m *multiplex) Close() error {
-	err := m.w.Close()
-	err2 := m.r.Close()
+	err := m.WriteCloser.Close()
+	err2 := m.ReadCloser.Close()
 	if err == nil {
 		err = err2
 	}
